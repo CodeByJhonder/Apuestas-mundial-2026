4cbaf25b374646b10ea5575d094b3e5d7d1f3a90
CodeByJhonder <jhondev20112025@gmail.com>
2026-07-09 12:52:27 -0400

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -25,5 +25,5 @@ const db = firebase.firestore();
  
 /* Nombre de la contraseña de administrador.
    CÁMBIALA antes de subir el proyecto a GitHub. */
-const ADMIN_PASSWORD = "1233214334";
+const ADMIN_PASSWORD = "J&Qwerty$$.31#";
  
